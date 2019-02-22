6d1f1c1bdcfe2a4fe383be1a0e17bae76a672156
owain.llewellyn <bilbercode@gmail.com>
2019-02-22 19:07:14 +0000

    feat: restore with selective collections

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ RUN apt-get update && apt-get -y install awscli
 
 RUN mkdir /telecom
 WORKDIR /telecom
-COPY . /telecom
+ADD run.sh /telecom/run.sh
 RUN chmod +x /telecom/run.sh
 
-CMD /telecom/run.sh
+ENTRYPOINT ["/telecom/run.sh"]
--- run.sh
+++ run.sh
@@ -8,21 +8,81 @@ if [[ -z "${BUCKET}" ]];then
     exit 1
 fi
 
-TIMESTAMP=`date +%Y-%m-%dT%H-%M-%S`
-BACKUP_NAME="$TIMESTAMP.dump.gz"
-S3PATH="s3://$BUCKET/$BACKUP_NAME"
 
-echo ${S3PATH}
+if [[ -z "${MONGO}" ]];then
+    echo "Missing required MONGO env VAR"
+    exit 1
+fi
 
-##
-# Create our mongo dump or exit
-#
-mongodump --uri ${MONGO} --gzip --archive=${BACKUP_NAME} ||  echo "Failed to create mongo dump!" && exit 1
-##
-# Move the backup to S3 or exit
-#
-aws s3 cp --sse AES256 ${BACKUP_NAME} ${S3PATH} || echo "Failed to copy mongo dump to AWS S3 bucket ${S3PATH}" && exit 1
-##
-# Success
-#
-echo "Sucessfully created backup ${BACKUP_NAME}, available at ${S3PATH}"
+
+function doBackup {
+
+    TIMESTAMP=`date +%Y-%m-%dT%H-%M-%S`
+    BACKUP_NAME="$TIMESTAMP.tar.gz"
+    S3PATH="s3://$BUCKET/$BACKUP_NAME"
+
+    ##
+    # Create our mongo dump into a timestamped directory
+    #
+    mongodump --uri ${MONGO} -o ${TIMESTAMP}
+
+    if [[ $? -ne 0 ]];then
+     echo "Failed to create mongo dump!"
+        exit 1
+    fi
+
+    ## package up the lot into a tar.gz
+    tar -zcvf ${BACKUP_NAME} ${TIMESTAMP}
+
+    ##
+    # Move the backup to S3 or exit
+    #
+    aws s3 cp --sse AES256 ${BACKUP_NAME} ${S3PATH}
+
+    if [[ $? -ne 0 ]];then
+     echo "Failed to copy mongo dump to AWS S3 bucket ${S3PATH}"
+        exit 1
+    fi
+    ##
+    # Success
+    #
+    echo "Successfully created backup ${BACKUP_NAME}, available at ${S3PATH}"
+}
+
+
+function doRestore {
+    BACKUP_NAME="${TIMESTAMP}.tar.gz"
+    S3PATH="s3://$BUCKET/$BACKUP_NAME"
+    mkdir restore
+
+    aws s3 cp ${S3PATH} ${BACKUP_NAME}
+    if [[ $? -ne 0 ]];then
+     echo "Failed to copy mongo dump from AWS S3 bucket ${S3PATH}"
+        exit 1
+    fi
+    tar -xzvf ${BACKUP_NAME}
+
+    for i in $(echo ${COLLECTIONS} | sed "s/,/ /g")
+    do
+        DATABASE=$(echo ${i} | awk -F "/" '{print $1}')
+        COLLECTION=$(echo ${i} | awk -F "/" '{print $2}')
+        mkdir -p restore/${DATABASE}
+        mv ${TIMESTAMP}/${i}.bson restore/${DATABASE}
+        mv ${TIMESTAMP}/${i}.metadata.json restore/${DATABASE}
+        echo ${i}
+    done
+    mongorestore --uri ${MONGO} --dir restore
+    echo "finished restore"
+}
+
+
+case $1 in
+    restore)
+        TIMESTAMP=$2
+        COLLECTIONS=$3
+        doRestore
+    ;;
+    *)
+        doBackup
+    ;;
+esac
